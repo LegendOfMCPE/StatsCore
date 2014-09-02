d4e47f0dcc1746c8305316d233bc12f405538411
PEMapModder <pemapmodder1970@gmail.com>
2014-09-02 19:08:09 +0800

    Allow JSON and YAML both

--- src/legendofmcpe/statscore/log/YAMLLog.php
+++ src/legendofmcpe/statscore/log/YAMLLog.php
@@ -2,19 +2,19 @@
 
 namespace legendofmcpe\statscore\log;
 
-class JSONLog extends NestedArrayLog{
+class YAMLLog extends NestedArrayLog{
 	/**
 	 * @param string $file
 	 * @return array a nested array from a log file
 	 */
 	protected function readFromFile($file){
-		return json_decode(file_get_contents($file));
+		return yaml_parse(file_get_contents($file));
 	}
 	/**
 	 * @param string $file
 	 * @param array $array
 	 */
 	protected function writeToFile($file, array $array){
-		file_put_contents($file, json_encode($array, JSON_BIGINT_AS_STRING | JSON_PRETTY_PRINT));
+		file_put_contents($file, yaml_emit($array, YAML_UTF8_ENCODING));
 	}
 }
