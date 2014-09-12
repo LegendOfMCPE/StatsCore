6be9bad209ce6f54af0d270bbb6caaa924d6a17f
PEMapModder <pemapmodder1970@gmail.com>
2014-09-12 19:12:43 +0800

    Now using custom pocketmine.yml

--- AutoKiller/src/LegendOfMCPE/AutoKiller/ServerKiller.php
+++ AutoKiller/src/LegendOfMCPE/AutoKiller/ServerKiller.php
@@ -7,6 +7,7 @@ use pocketmine\scheduler\PluginTask;
 
 class ServerKiller extends PluginTask{
 	public function onRun($ticks){
+		$this->getOwner()->getLogger()->alert("Trying to stop the server...");
 		$this->getOwner()->getServer()->dispatchCommand(new ConsoleCommandSender(), "stop");
 	}
 }
