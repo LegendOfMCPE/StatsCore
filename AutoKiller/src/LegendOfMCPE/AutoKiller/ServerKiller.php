e7929575232ecbb68613eb71d9eae2eb2365566a
PEMapModder <pemapmodder1970@gmail.com>
2014-10-02 12:22:55 +0800

    Echo to console

--- AutoKiller/src/LegendOfMCPE/AutoKiller/ServerKiller.php
+++ AutoKiller/src/LegendOfMCPE/AutoKiller/ServerKiller.php
@@ -8,6 +8,6 @@ use pocketmine\scheduler\PluginTask;
 class ServerKiller extends PluginTask{
 	public function onRun($ticks){
 		$this->getOwner()->getLogger()->alert("Checking status...");
-		if(count($this->getOwner()->getServer()->getPluginManager()->getPlugins()) >= 2) exit(0); else exit(2);
+		if(count($this->getOwner()->getServer()->getPluginManager()->getPlugins()) >= 2){ echo "success"; echo PHP_EOL; exit(0); } else{ echo "failure"; echo PHP_EOL; exit(2); }
 	}
 }
