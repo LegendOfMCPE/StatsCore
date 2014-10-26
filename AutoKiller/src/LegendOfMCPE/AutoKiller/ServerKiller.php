3d0812747d1e0ca733304c18ae3f11b99da5d38c
PEMapModder <pemapmodder1970@gmail.com>
2014-10-26 20:30:06 +0800

    Update ServerKiller.php

--- AutoKiller/src/LegendOfMCPE/AutoKiller/ServerKiller.php
+++ AutoKiller/src/LegendOfMCPE/AutoKiller/ServerKiller.php
@@ -8,6 +8,14 @@ use pocketmine\scheduler\PluginTask;
 class ServerKiller extends PluginTask{
 	public function onRun($ticks){
 		$this->getOwner()->getLogger()->alert("Checking status...");
-		if(count($this->getOwner()->getServer()->getPluginManager()->getPlugins()) >= 2){ echo "success"; echo PHP_EOL; exit(0); } else{ echo "failure"; echo PHP_EOL; exit(2); }
+		if(count($this->getOwner()->getServer()->getPluginManager()->getPlugins()) >= 2){
+			echo "success";
+			echo PHP_EOL;
+			exit(0);
+		} else{
+			echo "failure";
+			echo PHP_EOL;
+			exit(2);
+		}
 	}
 }
